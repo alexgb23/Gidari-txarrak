421abdcaa5d71d5d042ed602f76eeccfc9942e8a
Alex <alexander.galvez@estudioenpenascal.com>
2023-05-08 12:51:30 +0200

    12.51 pm

--- js/index.js
+++ js/index.js
@@ -29,16 +29,25 @@ function salir(){
 }
 
 /*validador contraseña*/
-let usuariosregistrados=[{correo:"alexander.galvez@estudioenpenascal.com",contrasena:1234},{correo:"gorka.jabonero@estudioenpenascal.com",contrasena:1234}]
+let usuariosregistrados=[{correo:"alexander.galvez@estudioenpenascal.com",contrasena:1234, nombre:"Alexander Galvez"},{correo:"gorka.jabonero@estudioenpenascal.com",contrasena:1234, nombre:"Gorka Jabonero"}]
 
 let correos=document.querySelector('#iCorreo');
 let password=document.querySelector('#iPass');
+let usuario
 
 function login(){
-    if (correos.value == usuariosregistrados[0].correo && password.value ==usuariosregistrados[0].contrasena) {
-        location.href = "html/reserva.html";
-      } else {
+    if (correos.value == usuariosregistrados[0].correo && password.value == usuariosregistrados[0].contrasena) {
+        // location.href = "html/reserva.html";
+        usuario=usuariosregistrados[0].nombre;
+        console.log(usuario);
+        }
+        else if(correos.value == usuariosregistrados[1].correo && password.value == usuariosregistrados[1].contrasena) {
+            // location.href = "html/reserva.html";
+            usuario=usuariosregistrados[1].nombre;
+            console.log(usuario);
+        }
+      
+      else 
         alert('usuario desconocido');
-       
-}
-}
+    }
+
