4a6404b63a2cb90a9f777af160d41bb8199ca53e
Soap <gorka.jabonero@estudioenpenascal.com>
2023-05-12 13:38:16 +0200

    Actualización de js, que casi funciona

--- js/index.js
+++ js/index.js
@@ -9,12 +9,12 @@ const modalContReserva = document.getElementById("reser-modal");
 const closeModalReserva = document.getElementById("close-res");
 
 //Modal index
-if (openModalIndex) {
-    openModalIndex.addEventListener('click', () => {
-        modalContIndex.classList.add('show');
-        
-    });
-}
+// if (openModalIndex) {
+//     openModalIndex.addEventListener('click', () => {
+//         modalContIndex.style.opacity='1';
+//         modalContIndex.style.pointerEvents = 'auto';
+//     });
+// }
 
 if (closeModalIndex) {
     closeModalIndex.addEventListener('click', () => {
@@ -26,7 +26,7 @@ if (closeModalIndex) {
 if (openModalReseva) {
     openModalReseva.addEventListener('click', () => {
         modalContReserva.classList.add('show');
-       
+
     });
 }
 if (closeModalReserva) {
@@ -35,6 +35,12 @@ if (closeModalReserva) {
     });
 }
 
+const tyu = document.querySelectorAll("#btn-siguiente, #btn-modal");
+
+tyu.addEventListener('click', funcion => {
+    modalContIndex.style.opacity='1';
+    modalContIndex.style.pointerEvents = 'auto';
+});
 
 /*aside-lateral*/
 let lateral = document.getElementById('lateral');
@@ -83,8 +89,6 @@ function guardarUsu() {
     let vNombre = document.registrar.nNombre.value;
 
 
-        alert("aaaaaaaaaaaaaaaa");
-
     if (vCorreo.trim() != "" && vContra.trim() != "" && vNombre.trim() != "") {
         usuariosregistrados.push({ correo: vCorreo, contrasena: vContra, nombre: vNombre, });
 
@@ -92,20 +96,19 @@ function guardarUsu() {
 
         var guardado = localStorage.getItem('My array');
         console.log('Valor obtenido: ', JSON.parse(guardado)); // esto es para ver si va  se van sumando las entradas
-       
+
 
         location.href = "../index.html";
-    } 
+    }
     else {
         alert("Introduzca todos los campos");
     }
-    
+
 }
 
-
-// function atras(){
-//     location.href = "../index.html";
-// }
+function atras() {
+    location.href = "../index.html";
+}
 
 /* Paginas reserva.js*/
 let dias;
@@ -118,19 +121,95 @@ function recogDatos(a) {
     dias = a;
 }
 
-
 function recogHora() {
-
+    console.log("*********************************************");
+    
+    // Paso 1: Guardo la fecha introducida en la variable fecha
     var hora = document.getElementById('btn-horas');
     var reserva = hora.options[hora.selectedIndex].value;
     fecha = dias + " " + mes + " " + ano + " " + reserva;
-    var data = new Date(fecha);
-    registrofinal.push(fecha);
-    console.log(registrofinal);
+    
+    console.log("Fecha: " + fecha);
 
-    //Guardamos la fecha en local y hacemos que el array se vaya agrandando a cada vez que introducimos nuevos
-    localStorage.setItem("Reservas", JSON.stringify(registrofinal));
+    //Me traigo todos los valores que tenga guardado en el array del localStorage
+    var registrofinal1 = JSON.parse(localStorage.getItem("Reservas"));
+    console.log('Valor en localStorage: ' + registrofinal1);
 
-        var guardado1 = localStorage.getItem('Reservas');
-        console.log('Valor obtenido: ', JSON.parse(guardado1));
+    //Recorro y comparo a ver si lo que está guardado en el localstorage coincide con la fecha nueva que estoy introduciendo
+    console.log("Anchura del array: " + registrofinal1.length);
+    for (let i = 0; i < registrofinal1.length; i++) {
+
+        //En caso de que ya exista esa fecha en el array devuelvo que es fecha ya existe
+        if ((fecha == registrofinal1[i])) {
+            alert("Hora y fecha ya reservada");
+        }
+
+        //En caso contrario, añadimos esta nueva fecha al array  
+        else {
+
+            //Metemos la nueva fecha en el array
+            registrofinal.push(fecha);
+
+            console.log("Cambio en el array: " + registrofinal);
+            //Subimos ese nuevo valor al localStorage
+            localStorage.setItem("Reservas", JSON.stringify(registrofinal));
+            console.log("Nueva reserva");
+            break;
+        }
+    }
 }
+
+//localStorage.removeItem('Reservas');
+
+
+
+/*************************************************************************** */
+/* Paginas reserva.js*/
+// let dias;
+// let mes = "Mayo";
+// let ano = "2023";
+// let fecha;
+// let registrofinal = new Array();
+
+// function recogDatos(a) {
+//     dias = a;
+// }
+
+// function recogHora() {
+//     console.log("*********************************************");
+    
+//     // Paso 1: Guardo la fecha introducida en la variable fecha
+//     var hora = document.getElementById('btn-horas');
+//     var reserva = hora.options[hora.selectedIndex].value;
+//     fecha = dias + " " + mes + " " + ano + " " + reserva;
+    
+//     console.log("Fecha: " + fecha);
+
+    
+   
+//     //Me traigo todos los valores que tenga guardado en el array del localStorage
+//     var registrofinal1 = JSON.parse(localStorage.getItem("Reservas"));
+//     console.log('Valor en localStorage: ' + registrofinal1);
+
+//     //Recorro y comparo a ver si lo que está guardado en el localstorage coincide con la fecha nueva que estoy introduciendo
+//     console.log("Anchura del array: " + registrofinal1.length);
+//     for (let i = 0; i < registrofinal1.length; i++) {
+
+//         //En caso de que ya exista esa fecha en el array devuelvo que es fecha ya existe
+//         if ((fecha == registrofinal1)) {
+//             alert("Hora y fecha ya reservada");
+//         }
+
+//         //En caso contrario, añadimos esta nueva fecha al array  
+//         else {
+
+//             //Metemos la nueva fecha en el array
+//             registrofinal.push(fecha);
+
+//             console.log("Cambio en el array: " + registrofinal);
+//             //Subimos ese nuevo valor al localStorage
+//             localStorage.setItem("Reservas", JSON.stringify(registrofinal));
+//             console.log("Nueva reserva");
+//         }
+//     }
+// }
